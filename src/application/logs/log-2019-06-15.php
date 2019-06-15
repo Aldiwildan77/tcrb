f99fdfbacffdfb9681b84577cf7549365c36d1f1
Muhammad Wildan Aldiansyah <aldiwild77@gmail.com>
2019-06-15 09:10:19 +0700

    adding logs message

---
+++ src/application/logs/log-2019-06-15.php
@@ -0,0 +1,189 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+
+ERROR - 2019-06-15 03:56:31 --> $config['composer_autoload'] is set to TRUE but C:\xampp\htdocs\tcrb\src\application\vendor/autoload.php was not found.
+INFO - 2019-06-15 03:56:31 --> Config Class Initialized
+INFO - 2019-06-15 03:56:31 --> Hooks Class Initialized
+DEBUG - 2019-06-15 03:56:31 --> UTF-8 Support Enabled
+INFO - 2019-06-15 03:56:31 --> Utf8 Class Initialized
+INFO - 2019-06-15 03:56:31 --> URI Class Initialized
+INFO - 2019-06-15 03:56:31 --> Router Class Initialized
+INFO - 2019-06-15 03:56:31 --> Output Class Initialized
+INFO - 2019-06-15 03:56:31 --> Security Class Initialized
+DEBUG - 2019-06-15 03:56:31 --> Global POST, GET and COOKIE data sanitized
+INFO - 2019-06-15 03:56:31 --> Input Class Initialized
+INFO - 2019-06-15 03:56:31 --> Language Class Initialized
+INFO - 2019-06-15 03:56:31 --> Loader Class Initialized
+INFO - 2019-06-15 03:56:31 --> Helper loaded: url_helper
+INFO - 2019-06-15 03:56:31 --> Database Driver Class Initialized
+INFO - 2019-06-15 03:56:31 --> Session: Class initialized using 'database' driver.
+INFO - 2019-06-15 03:56:31 --> Helper loaded: form_helper
+INFO - 2019-06-15 03:56:31 --> Form Validation Class Initialized
+INFO - 2019-06-15 03:56:31 --> Controller Class Initialized
+INFO - 2019-06-15 03:56:31 --> Model "LoginModel" initialized
+INFO - 2019-06-15 08:56:32 --> File loaded: C:\xampp\htdocs\tcrb\src\application\views\templates/header.php
+INFO - 2019-06-15 08:56:32 --> File loaded: C:\xampp\htdocs\tcrb\src\application\views\login/login_view.php
+INFO - 2019-06-15 08:56:32 --> File loaded: C:\xampp\htdocs\tcrb\src\application\views\templates/footer.php
+INFO - 2019-06-15 08:56:32 --> Final output sent to browser
+DEBUG - 2019-06-15 08:56:32 --> Total execution time: 0.2024
+ERROR - 2019-06-15 03:57:27 --> $config['composer_autoload'] is set to TRUE but C:\xampp\htdocs\tcrb\src\application\vendor/autoload.php was not found.
+INFO - 2019-06-15 03:57:27 --> Config Class Initialized
+INFO - 2019-06-15 03:57:27 --> Hooks Class Initialized
+DEBUG - 2019-06-15 03:57:27 --> UTF-8 Support Enabled
+INFO - 2019-06-15 03:57:27 --> Utf8 Class Initialized
+INFO - 2019-06-15 03:57:27 --> URI Class Initialized
+INFO - 2019-06-15 03:57:27 --> Router Class Initialized
+INFO - 2019-06-15 03:57:27 --> Output Class Initialized
+INFO - 2019-06-15 03:57:27 --> Security Class Initialized
+DEBUG - 2019-06-15 03:57:27 --> Global POST, GET and COOKIE data sanitized
+INFO - 2019-06-15 03:57:27 --> Input Class Initialized
+INFO - 2019-06-15 03:57:27 --> Language Class Initialized
+INFO - 2019-06-15 03:57:27 --> Loader Class Initialized
+INFO - 2019-06-15 03:57:27 --> Helper loaded: url_helper
+INFO - 2019-06-15 03:57:27 --> Database Driver Class Initialized
+INFO - 2019-06-15 03:57:27 --> Session: Class initialized using 'database' driver.
+INFO - 2019-06-15 03:57:27 --> Helper loaded: form_helper
+INFO - 2019-06-15 03:57:27 --> Form Validation Class Initialized
+INFO - 2019-06-15 03:57:27 --> Controller Class Initialized
+INFO - 2019-06-15 03:57:27 --> Model "LoginModel" initialized
+INFO - 2019-06-15 08:57:27 --> File loaded: C:\xampp\htdocs\tcrb\src\application\views\templates/header.php
+INFO - 2019-06-15 08:57:27 --> File loaded: C:\xampp\htdocs\tcrb\src\application\views\login/recovery_view.php
+INFO - 2019-06-15 08:57:27 --> File loaded: C:\xampp\htdocs\tcrb\src\application\views\templates/footer.php
+INFO - 2019-06-15 08:57:27 --> Final output sent to browser
+DEBUG - 2019-06-15 08:57:27 --> Total execution time: 0.1956
+ERROR - 2019-06-15 03:57:41 --> $config['composer_autoload'] is set to TRUE but C:\xampp\htdocs\tcrb\src\application\vendor/autoload.php was not found.
+INFO - 2019-06-15 03:57:41 --> Config Class Initialized
+INFO - 2019-06-15 03:57:41 --> Hooks Class Initialized
+DEBUG - 2019-06-15 03:57:41 --> UTF-8 Support Enabled
+INFO - 2019-06-15 03:57:41 --> Utf8 Class Initialized
+INFO - 2019-06-15 03:57:41 --> URI Class Initialized
+INFO - 2019-06-15 03:57:41 --> Router Class Initialized
+INFO - 2019-06-15 03:57:41 --> Output Class Initialized
+INFO - 2019-06-15 03:57:41 --> Security Class Initialized
+DEBUG - 2019-06-15 03:57:41 --> Global POST, GET and COOKIE data sanitized
+INFO - 2019-06-15 03:57:41 --> Input Class Initialized
+INFO - 2019-06-15 03:57:41 --> Language Class Initialized
+INFO - 2019-06-15 03:57:41 --> Loader Class Initialized
+INFO - 2019-06-15 03:57:41 --> Helper loaded: url_helper
+INFO - 2019-06-15 03:57:41 --> Database Driver Class Initialized
+INFO - 2019-06-15 03:57:41 --> Session: Class initialized using 'database' driver.
+INFO - 2019-06-15 03:57:41 --> Helper loaded: form_helper
+INFO - 2019-06-15 03:57:41 --> Form Validation Class Initialized
+INFO - 2019-06-15 03:57:41 --> Controller Class Initialized
+INFO - 2019-06-15 03:57:41 --> Model "LoginModel" initialized
+INFO - 2019-06-15 08:57:41 --> File loaded: C:\xampp\htdocs\tcrb\src\application\views\templates/header.php
+INFO - 2019-06-15 08:57:41 --> File loaded: C:\xampp\htdocs\tcrb\src\application\views\login/recovery_view.php
+INFO - 2019-06-15 08:57:41 --> File loaded: C:\xampp\htdocs\tcrb\src\application\views\templates/footer.php
+INFO - 2019-06-15 08:57:41 --> Final output sent to browser
+DEBUG - 2019-06-15 08:57:41 --> Total execution time: 0.2040
+ERROR - 2019-06-15 03:57:44 --> $config['composer_autoload'] is set to TRUE but C:\xampp\htdocs\tcrb\src\application\vendor/autoload.php was not found.
+INFO - 2019-06-15 03:57:44 --> Config Class Initialized
+INFO - 2019-06-15 03:57:44 --> Hooks Class Initialized
+DEBUG - 2019-06-15 03:57:44 --> UTF-8 Support Enabled
+INFO - 2019-06-15 03:57:44 --> Utf8 Class Initialized
+INFO - 2019-06-15 03:57:44 --> URI Class Initialized
+DEBUG - 2019-06-15 03:57:44 --> No URI present. Default controller set.
+INFO - 2019-06-15 03:57:44 --> Router Class Initialized
+INFO - 2019-06-15 03:57:44 --> Output Class Initialized
+INFO - 2019-06-15 03:57:44 --> Security Class Initialized
+DEBUG - 2019-06-15 03:57:44 --> Global POST, GET and COOKIE data sanitized
+INFO - 2019-06-15 03:57:44 --> Input Class Initialized
+INFO - 2019-06-15 03:57:44 --> Language Class Initialized
+INFO - 2019-06-15 03:57:44 --> Loader Class Initialized
+INFO - 2019-06-15 03:57:44 --> Helper loaded: url_helper
+INFO - 2019-06-15 03:57:44 --> Database Driver Class Initialized
+INFO - 2019-06-15 03:57:44 --> Session: Class initialized using 'database' driver.
+INFO - 2019-06-15 03:57:44 --> Helper loaded: form_helper
+INFO - 2019-06-15 03:57:44 --> Form Validation Class Initialized
+INFO - 2019-06-15 03:57:44 --> Controller Class Initialized
+INFO - 2019-06-15 03:57:44 --> File loaded: C:\xampp\htdocs\tcrb\src\application\views\comingSoon/comingSoon_view.php
+INFO - 2019-06-15 03:57:44 --> Final output sent to browser
+DEBUG - 2019-06-15 03:57:44 --> Total execution time: 0.1819
+ERROR - 2019-06-15 03:57:47 --> $config['composer_autoload'] is set to TRUE but C:\xampp\htdocs\tcrb\src\application\vendor/autoload.php was not found.
+INFO - 2019-06-15 03:57:47 --> Config Class Initialized
+INFO - 2019-06-15 03:57:47 --> Hooks Class Initialized
+DEBUG - 2019-06-15 03:57:47 --> UTF-8 Support Enabled
+INFO - 2019-06-15 03:57:47 --> Utf8 Class Initialized
+INFO - 2019-06-15 03:57:47 --> URI Class Initialized
+INFO - 2019-06-15 03:57:47 --> Router Class Initialized
+INFO - 2019-06-15 03:57:47 --> Output Class Initialized
+INFO - 2019-06-15 03:57:47 --> Security Class Initialized
+DEBUG - 2019-06-15 03:57:47 --> Global POST, GET and COOKIE data sanitized
+INFO - 2019-06-15 03:57:47 --> Input Class Initialized
+INFO - 2019-06-15 03:57:47 --> Language Class Initialized
+INFO - 2019-06-15 03:57:47 --> Loader Class Initialized
+INFO - 2019-06-15 03:57:47 --> Helper loaded: url_helper
+INFO - 2019-06-15 03:57:47 --> Database Driver Class Initialized
+INFO - 2019-06-15 03:57:47 --> Session: Class initialized using 'database' driver.
+INFO - 2019-06-15 03:57:47 --> Helper loaded: form_helper
+INFO - 2019-06-15 03:57:47 --> Form Validation Class Initialized
+INFO - 2019-06-15 03:57:47 --> Controller Class Initialized
+INFO - 2019-06-15 03:57:47 --> File loaded: C:\xampp\htdocs\tcrb\src\application\views\comingSoon/comingSoon_form.php
+INFO - 2019-06-15 03:57:47 --> Final output sent to browser
+DEBUG - 2019-06-15 03:57:47 --> Total execution time: 0.2084
+ERROR - 2019-06-15 03:59:05 --> $config['composer_autoload'] is set to TRUE but C:\xampp\htdocs\tcrb\src\application\vendor/autoload.php was not found.
+INFO - 2019-06-15 03:59:05 --> Config Class Initialized
+INFO - 2019-06-15 03:59:05 --> Hooks Class Initialized
+DEBUG - 2019-06-15 03:59:05 --> UTF-8 Support Enabled
+INFO - 2019-06-15 03:59:05 --> Utf8 Class Initialized
+INFO - 2019-06-15 03:59:05 --> URI Class Initialized
+INFO - 2019-06-15 03:59:05 --> Router Class Initialized
+INFO - 2019-06-15 03:59:05 --> Output Class Initialized
+INFO - 2019-06-15 03:59:05 --> Security Class Initialized
+DEBUG - 2019-06-15 03:59:05 --> Global POST, GET and COOKIE data sanitized
+INFO - 2019-06-15 03:59:05 --> Input Class Initialized
+INFO - 2019-06-15 03:59:05 --> Language Class Initialized
+INFO - 2019-06-15 03:59:05 --> Loader Class Initialized
+INFO - 2019-06-15 03:59:05 --> Helper loaded: url_helper
+INFO - 2019-06-15 03:59:05 --> Database Driver Class Initialized
+INFO - 2019-06-15 03:59:05 --> Session: Class initialized using 'database' driver.
+INFO - 2019-06-15 03:59:05 --> Helper loaded: form_helper
+INFO - 2019-06-15 03:59:05 --> Form Validation Class Initialized
+INFO - 2019-06-15 03:59:05 --> Controller Class Initialized
+INFO - 2019-06-15 03:59:05 --> File loaded: C:\xampp\htdocs\tcrb\src\application\views\comingSoon/comingSoon_form.php
+INFO - 2019-06-15 03:59:05 --> Final output sent to browser
+DEBUG - 2019-06-15 03:59:05 --> Total execution time: 0.1757
+ERROR - 2019-06-15 03:59:23 --> $config['composer_autoload'] is set to TRUE but C:\xampp\htdocs\tcrb\src\application\vendor/autoload.php was not found.
+INFO - 2019-06-15 03:59:23 --> Config Class Initialized
+INFO - 2019-06-15 03:59:23 --> Hooks Class Initialized
+DEBUG - 2019-06-15 03:59:23 --> UTF-8 Support Enabled
+INFO - 2019-06-15 03:59:23 --> Utf8 Class Initialized
+INFO - 2019-06-15 03:59:23 --> URI Class Initialized
+DEBUG - 2019-06-15 03:59:23 --> No URI present. Default controller set.
+INFO - 2019-06-15 03:59:23 --> Router Class Initialized
+INFO - 2019-06-15 03:59:23 --> Output Class Initialized
+INFO - 2019-06-15 03:59:23 --> Security Class Initialized
+DEBUG - 2019-06-15 03:59:23 --> Global POST, GET and COOKIE data sanitized
+INFO - 2019-06-15 03:59:23 --> Input Class Initialized
+INFO - 2019-06-15 03:59:23 --> Language Class Initialized
+INFO - 2019-06-15 03:59:23 --> Loader Class Initialized
+INFO - 2019-06-15 03:59:23 --> Helper loaded: url_helper
+INFO - 2019-06-15 03:59:23 --> Database Driver Class Initialized
+INFO - 2019-06-15 03:59:23 --> Session: Class initialized using 'database' driver.
+INFO - 2019-06-15 03:59:23 --> Helper loaded: form_helper
+INFO - 2019-06-15 03:59:23 --> Form Validation Class Initialized
+INFO - 2019-06-15 03:59:23 --> Controller Class Initialized
+INFO - 2019-06-15 03:59:23 --> File loaded: C:\xampp\htdocs\tcrb\src\application\views\comingSoon/comingSoon_view.php
+INFO - 2019-06-15 03:59:23 --> Final output sent to browser
+DEBUG - 2019-06-15 03:59:23 --> Total execution time: 0.1864
+ERROR - 2019-06-15 04:05:11 --> $config['composer_autoload'] is set to TRUE but C:\xampp\htdocs\tcrb\src\application\vendor/autoload.php was not found.
+INFO - 2019-06-15 04:05:11 --> Config Class Initialized
+INFO - 2019-06-15 04:05:11 --> Hooks Class Initialized
+DEBUG - 2019-06-15 04:05:11 --> UTF-8 Support Enabled
+INFO - 2019-06-15 04:05:11 --> Utf8 Class Initialized
+INFO - 2019-06-15 04:05:11 --> URI Class Initialized
+INFO - 2019-06-15 04:05:11 --> Router Class Initialized
+INFO - 2019-06-15 04:05:11 --> Output Class Initialized
+INFO - 2019-06-15 04:05:11 --> Security Class Initialized
+DEBUG - 2019-06-15 04:05:11 --> Global POST, GET and COOKIE data sanitized
+INFO - 2019-06-15 04:05:11 --> Input Class Initialized
+INFO - 2019-06-15 04:05:11 --> Language Class Initialized
+INFO - 2019-06-15 04:05:11 --> Loader Class Initialized
+INFO - 2019-06-15 04:05:11 --> Helper loaded: url_helper
+INFO - 2019-06-15 04:05:11 --> Database Driver Class Initialized
+INFO - 2019-06-15 04:05:11 --> Session: Class initialized using 'database' driver.
+INFO - 2019-06-15 04:05:11 --> Helper loaded: form_helper
+INFO - 2019-06-15 04:05:11 --> Form Validation Class Initialized
+INFO - 2019-06-15 04:05:11 --> Controller Class Initialized
+INFO - 2019-06-15 04:05:11 --> File loaded: C:\xampp\htdocs\tcrb\src\application\views\comingSoon/comingSoon_hasil.php
+INFO - 2019-06-15 04:05:11 --> Final output sent to browser
+DEBUG - 2019-06-15 04:05:11 --> Total execution time: 0.2239
