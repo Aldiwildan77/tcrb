f4231c730a32ec21597f2b7e3db6148bb11a944c
Muhammad Wildan Aldiansyah <aldiwild77@gmail.com>
2019-06-18 22:25:14 +0700

    add logging for env

--- src/application/config/hooks.php
+++ src/application/config/hooks.php
@@ -1,8 +1,9 @@
 <?php
-defined('BASEPATH') OR exit('No direct script access allowed');
+defined('BASEPATH') or exit('No direct script access allowed');
 
 // for environment hooks
-$hook['pre_system'] = function() {
-  $dotenv = Dotenv\Dotenv::create(FCPATH);
-  $dotenv->overload();
+$hook['pre_system'] = function () {
+	$dotenv = Dotenv\Dotenv::create(FCPATH);
+	$dotenv->overload();
+	log_message('INFO', 'Environment successfully loaded');
 };
