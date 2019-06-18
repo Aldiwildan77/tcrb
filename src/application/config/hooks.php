2a1689f687181b3da83e86ed96a1347d6711ec2f
Muhammad Wildan Aldiansyah <aldiwild77@gmail.com>
2019-06-18 17:55:33 +0700

    adding hooks and env

--- src/application/config/hooks.php
+++ src/application/config/hooks.php
@@ -13,6 +13,6 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 */
 
 $hook['pre_system'] = function() {
-  $dotenv = Dotenv\Dotenv::create('');
+  $dotenv = Dotenv\Dotenv::create(FCPATH);
   $dotenv->load();
 };
