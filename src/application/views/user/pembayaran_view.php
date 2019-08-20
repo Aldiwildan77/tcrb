84a5804e3766615e1772c4a2c2fa793e1dee5970
Muhammad Wildan Aldiansyah <aldiwild77@gmail.com>
2019-08-20 22:45:50 +0700

    pembayaran view added

---
+++ src/application/views/user/pembayaran_view.php
@@ -0,0 +1,15 @@
+<div class="col-lg-8 offset-lg-1 user pt-5">
+	<div class="row">
+		<div class="col-12">
+			<h3 class="text-center"><b>Pembayaran</b></h3>
+		</div>
+	</div>
+</div>
+
+
+
+
+<!-- untuk aside 3 div,   -->
+</div>
+</div>
+</div>
