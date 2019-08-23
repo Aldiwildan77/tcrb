f4117280ad1e35f177e8b8bc829baa166ff7cef7
Alif Andarta <alifandartaa@yahoo.com>
2019-08-23 12:28:24 +0700

    pembayaran view

--- src/application/views/user/pembayaran_view.php
+++ src/application/views/user/pembayaran_view.php
@@ -1,13 +1,138 @@
-<div class="col-lg-8 offset-lg-1 user pt-5">
-	<div class="row">
+<div class="col-lg-8 offset-lg-1">
+	<div class="col-lg-12 user pt-3">
+		<div class="row mb-4">
+			<div class="col-12">
+				<h3 class="text-center"><b>Note</b></h3>
+			</div>
+		</div>
+	</div>
+	<div class="user p-4">
+		<!-- <div class="row mb-4">
+			<div class="col-12">
+				<h3 class="text-center"><b>Pembayaran</b></h3>
+			</div>
+		</div> -->
+		<!-- <div class="row mb-5">
 		<div class="col-12">
 			<h3 class="text-center"><b>Pembayaran</b></h3>
 		</div>
+	</div> -->
+		<!-- <div class="row mx-auto">
+		<img src="<?= base_url('assets/img/line.png') ?>" class="rounded-circle float-left" width="100" height="100" alt="#">
+	</div> -->
+		<div class="row table-responsive">
+			<!-- <div class="col"> -->
+			<table class="table table-borderless">
+				<tbody>
+					<tr>
+						<th>Nama</th>
+						<td colspan="3">Alif</td>
+						<th>Status</th>
+						<td><span class="badge badge-success">Success</span></td>
+					</tr>
+					<tr>
+						<th>Instansi</th>
+						<td colspan="3">Team Liquid</td>
+						<th>Tanggal Pembayaran</th>
+						<td>1 November 2019</td>
+					</tr>
+					<tr>
+						<th>Telepon</th>
+						<td>081217915595</td>
+					</tr>
+					<tr>
+					</tr>
+					<tr>
+					</tr>
+				</tbody>
+			</table>
+			<!-- <p> Nama : </p>
+			<p>Instansi : </p>
+			<p>Telp : </p> -->
+			<!-- </div> -->
+			<!-- <div class="col">
+			<p>Status : <span class="badge badge-success">Success</span></p>
+			<p>Tanggal Pembayaran : </p>
+		</div> -->
+		</div>
+		<!-- <hr style="border: 5px solid #343A40;
+	border-radius: 4px;"> -->
+		<div class="row justify-content-center mx-auto">
+			<!-- Tabel perorangan -->
+			<table class="table table-striped table-borderless table-sm">
+				<thead class="thead-dark">
+					<tr>
+						<th scope="col">No</th>
+						<th scope="col">Nama</th>
+						<th scope="col">Kategori</th>
+						<th scope="col">Harga(IDR)</th>
+					</tr>
+				</thead>
+				<tbody>
+					<tr>
+						<th scope="row">1</th>
+						<td>Mark</td>
+						<td>Otto</td>
+						<td> Ceb</td>
+					</tr>
+					<tr>
+						<th scope="row">2</th>
+						<td>Jacob</td>
+						<td>Thornton</td>
+						<td> Topson</td>
+					</tr>
+					<tr>
+						<th scope="row"> </th>
+						<td class="bg-light"> </td>
+						<td class="bg-light" style="text-align:right;">Total : </td>
+						<td class="bg-dark"> </td>
+					</tr>
+				</tbody>
+			</table>
+			<!-- Tabel beregu -->
+			<table class="table table-striped table-borderless table-sm">
+				<thead class="thead-dark">
+					<tr>
+						<th scope="col">No</th>
+						<th scope="col">Nama Regu</th>
+						<th scope="col">Nama Pemain</th>
+						<th scope="col">Harga(IDR)</th>
+					</tr>
+				</thead>
+				<tbody>
+					<tr>
+						<th scope="row">1</th>
+						<td>Mark</td>
+						<td>Otto</td>
+						<td> Ceb</td>
+					</tr>
+					<tr>
+						<th scope="row">2</th>
+						<td>Jacob</td>
+						<td>Thornton</td>
+						<td> Topson</td>
+					</tr>
+					<tr>
+						<th scope="row"> </th>
+						<td class="bg-light"> </td>
+						<td class="bg-light" style="text-align:right;">Total : </td>
+						<td class="bg-dark"> </td>
+					</tr>
+				</tbody>
+			</table>
+			<button type="button" class="btn btn-info btn-lg mt-2" style="border-radius:25px;">Cetak Struk</button>
+			<div class="input-group my-3">
+				<div class="custom-file">
+					<input type="file" class="custom-file-input" id="inputGroupFile02">
+					<label class="custom-file-label" for="inputGroupFile02" aria-describedby="inputGroupFileAddon02">Choose file</label>
+				</div>
+				<div class="input-group-append">
+					<span class="input-group-text" id="inputGroupFileAddon02">Upload</span>
+				</div>
+			</div>
+		</div>
 	</div>
 </div>
-
-
-
 
 <!-- untuk aside 3 div,   -->
 </div>
