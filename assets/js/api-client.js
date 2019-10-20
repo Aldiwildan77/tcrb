09ed07252efa81856777ec1a9fc7fe53e30210cd
Fakhri Imaduddin <fakhriimaduddin19@gmail.com>
2019-10-20 23:14:46 +0700

    add jspdf 90% done (kurang qrcode);

--- assets/js/api-client.js
+++ assets/js/api-client.js
@@ -49,48 +49,145 @@ const loadQrCode = ({ }, params) => {
 const generatePdfPerorangan = (username, token, rawData) => {
 	const url = window.location.origin + '/tcrb/'
 	let data = jQuery.parseJSON(rawData)
-	// console.log(data)
-	// return
-	// console.log(data['user']['nama_lengkap'])
 	var doc = new jsPDF('p', 'mm', 'a4', true)
 
 	var img = new Image()
 	img.src = url + 'assets/img/pdf/perorangan.png'
 	doc.addFileToVFS('../fonts/Arial.ttf', 'Arial');
-	doc.addFont('Arial', 'Arial', '');
-	doc.setFont('Arial', 'normal')
+	doc.addFont('Arial', 'arial', 'normal');
+	doc.setFont('arial', 'normal')
 	doc.setFontSize(12)
 	doc.addImage(img, 'PNG', 0, 0, 210, 297, undefined, 'FAST')
-	doc.text(data['user']['nama_lengkap'], 71, 49.5)
-	doc.text(data['user']['instansi'], 71, 56.5)
+	doc.text(data['user']['nama_lengkap'], 72, 50.5)
+	doc.text(data['user']['instansi'], 72, 57.5)
+	doc.text(data['user']['no_telepon'], 72, 65.5)
+	doc.text(data['status']['tanggal_bayar'], 72, 73.5)
 	// img.src = url + 'generate-qr'
+	let pemain = []
+	for (let index = 0; index < data['pemain'].length; index++) {
+		let row = [index + 1, data['pemain'][index]['nama_pemain'], data['pemain'][index]['nama_kategori']]
+		pemain.push(row);
+	}
 
-	// doc.autoTable({
-	// 	theme: 'plain',
-	// 	headStyles: {
-	// 		fillColor: [100, 100, 100],
-	// 		lineWidth: 0.3,
-	// 		lineColor: 0
-	// 	},
-	// 	startY: 50,
-	// 	head: [['No', 'Nama', 'Alamat']],
-	// 	bodyStyles: {
-	// 		fillColor: [255, 255, 255],
-	// 		lineWidth: 0.3,
-	// 		lineColor: 0
-	// 	},
-	// 	body: [
-	// 		['1', 'Fakhri', 'Malang'],
-	// 		['2', 'Dia', 'Malang juga']
-	// 	]
-	// })
-	// doc.save("Bukti-Pendaftaran-TCRB-" + username + ".pdf")
-	var string = doc.output('datauristring')
-	var embed = "<embed width='100%' height='100%' src='" + string + "'/>"
-	var x = window.open();
-	x.document.open();
-	x.document.write(embed);
-	x.document.close();
+	doc.autoTable({
+		theme: 'plain',
+		headStyles: {
+			fillColor: [100, 100, 100],
+			lineWidth: 0.3,
+			lineColor: 0,
+			halign: 'center'
+		},
+		startY: 107,
+		margin: 23,
+		head: [['No', 'Nama Pemain', 'Kategori']],
+		bodyStyles: {
+			fillColor: [255, 255, 255],
+			lineWidth: 0.3,
+			lineColor: 0
+		},
+		tableWidth: 165,
+		body: pemain
+	})
+	doc.save("Bukti-Pendaftaran-TCRB-" + username + ".pdf")
+	// var string = doc.output('datauristring')
+	// var embed = "<embed width='100%' height='100%' src='" + string + "'/>"
+	// var x = window.open();
+	// x.document.open();
+	// x.document.write(embed);
+	// x.document.close();
+	$('.loading').html('<h3 class="text-center">PDF selesai dibuat</h3>');
+	Swal.fire({
+		type: 'success',
+		title: 'Selesai',
+		text: 'PDF telah selesai dibuat'
+	})
+}
+
+const generatePdfBeregu = (username, token, rawData) => {
+	const url = window.location.origin + '/tcrb/'
+	let data = jQuery.parseJSON(rawData)
+	var doc = new jsPDF('p', 'mm', 'a4', true)
+
+	var img = new Image()
+	img.src = url + 'assets/img/pdf/beregu.png'
+	doc.addFileToVFS('../fonts/Arial.ttf', 'Arial');
+	doc.addFont('Arial', 'arial', 'normal');
+	doc.setFont('arial', 'normal')
+	doc.setFontSize(12)
+	doc.addImage(img, 'PNG', 0, 0, 210, 297, undefined, 'FAST')
+	doc.text(data['user']['nama_lengkap'], 72, 50.5)
+	doc.text(data['user']['instansi'], 72, 57.5)
+	doc.text(data['user']['no_telepon'], 72, 65.5)
+	doc.text(data['regu'][0]['tanggal_bayar'], 72, 73.5)
+	// img.src = url + 'generate-qr'
+	let regu = []
+	for (let index = 0; index < data['regu'].length; index++) {
+		let row = [index + 1, data['regu'][index]['nama'], data['regu'][index]['namaPaket']]
+		regu.push(row);
+	}
+
+	doc.autoTable({
+		theme: 'plain',
+		headStyles: {
+			fillColor: [100, 100, 100],
+			lineWidth: 0.3,
+			lineColor: 0
+		},
+		startY: 107,
+		margin: 23,
+		head: [['No', 'Nama Regu', 'Kategori']],
+		bodyStyles: {
+			fillColor: [255, 255, 255],
+			lineWidth: 0.3,
+			lineColor: 0
+		},
+		tableWidth: 165,
+		body: regu
+	})
+
+	if(data['jumlahOfficial'].length > 0){
+		let official = []
+		for (let index = 0; index < data['official'].length; index++) {
+			if(data['official'][index]['nama'] == null || data['official'][index]['nama'] === "" ){
+				continue
+			}
+				let row = [index + 1, data['official'][index]['nama'], data['official'][index]['namaPaket']]
+				official.push(row);
+		}
+
+		doc.autoTable({
+			theme: 'plain',
+			headStyles: {
+				fillColor: [100, 100, 100],
+				lineWidth: 0.3,
+				lineColor: 0
+			},
+			startY: 221,
+			margin: 23,
+			head: [['No', 'Nama Official', 'Kategori']],
+			bodyStyles: {
+				fillColor: [255, 255, 255],
+				lineWidth: 0.3,
+				lineColor: 0
+			},
+			tableWidth: 165,
+			body: official
+		})
+	}
+
+	doc.save("Bukti-Pendaftaran-TCRB-" + username + ".pdf")
+	// var string = doc.output('datauristring')
+	// var embed = "<embed width='100%' height='100%' src='" + string + "'/>"
+	// var x = window.open();
+	// x.document.open();
+	// x.document.write(embed);
+	// x.document.close();
+	$('.loading').html('<h3 class="text-center">PDF selesai dibuat</h3>');
+	Swal.fire({
+		type: 'success',
+		title: 'Selesai',
+		text: 'PDF telah selesai dibuat'
+	})
 }
 
 // function generatePdf(username){
